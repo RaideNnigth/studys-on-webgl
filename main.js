086a16414a69a16ad021be6c88869d53725275b0
Gustavo Pereira <gustavo.pinzon55@gmail.com>
2025-05-25 17:40:25 -0300

    Interpolated triangle

--- main.js
+++ main.js
@@ -15,14 +15,31 @@ const vertexData = [
       -1, -1,  0,
 ]
 
-const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+const colorData = [
+ // r  g  b
+    1, 0, 0,
+    0, 1, 0,
+    0, 0, 1,
+]
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
+precision mediump float;
+
 attribute vec3 position;
+attribute vec3 color;
+varying vec3 vColor;
+
 void main(){
+    vColor = color;
     gl_Position = vec4(position, 1);
 }    
 `);
@@ -34,8 +51,11 @@ if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader,`
+precision mediump float;
+
+varying vec3 vColor;
 void main() {
-    gl_FragColor = vec4(1, 0, 0, 1);
+    gl_FragColor = vec4(vColor, 1);
 }    
 `);
 gl.compileShader(fragmentShader);
@@ -55,7 +75,13 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 
 const positionLocation = gl.getAttribLocation(program, `position`);
 gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+const colorLocation = gl.getAttribLocation(program, `color`);
+gl.enableVertexAttribArray(colorLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
 gl.useProgram(program);
 gl.drawArrays(gl.TRIANGLES, 0, 3);
